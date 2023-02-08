13a0093a1400a6a9d62c1e13d7e338852df19ea5
Josue Hernandez <josuehdz0@gmail.com>
2023-02-08 15:28:42 -0700

    Able to get Pokémon from API, now making template for draw

--- app/Services/PokemonsService.js
+++ app/Services/PokemonsService.js
@@ -3,9 +3,9 @@ import { pokeApi } from "./AxiosService.js";
 
 class PokemonsService{
   async getPokemons() {
-    const res = await pokeApi.get()
-    console.log('get pokemon', res.data);
-    appState.
+    const res = await pokeApi.get('pokemon?limit=100000&offset=0')
+    console.log('get pokemon', res.data.results);
+    appState.pokemons = res.data.results
   }
 
 }
