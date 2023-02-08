6a928f3a9019941574c65cbe1b696c3fcf80b602
Josue Hernandez <josuehdz0@gmail.com>
2023-02-08 16:23:06 -0700

    Drawing works, gotta make card scrollable?

--- app/Controllers/PokemonsController.js
+++ app/Controllers/PokemonsController.js
@@ -1,8 +1,20 @@
+import { appState } from "../AppState.js";
+import { Pokemon } from "../Models/Pokemon.js";
 import { pokemonsService } from "../Services/PokemonsService.js";
 import { Pop } from "../Utils/Pop.js";
+import { setHTML } from "../Utils/Writer.js";
+
+
+function _drawPokemons(){
+  let template = ''
+  appState.pokemons.forEach(p => template += Pokemon.PokemonListTemplate(p))
+  setHTML('pokemons',template)
+}
 
 export class PokemonsController{
   constructor (){
+    this.getPokemons()
+    appState.on('pokemons', _drawPokemons)
 
   }
 
--- app/Models/Pokemon.js
+++ app/Models/Pokemon.js
@@ -3,4 +3,13 @@ export class Pokemon{
     this.name = data.name
   }
 
+
+  static PokemonListTemplate(pokemon){
+    return`
+    <p>
+    <i class="mdi mdi-pokeball f-18"></i> ${pokemon.name}
+  </p>
+    `
+  }
+
 }
