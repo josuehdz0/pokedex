9cd74de69564935db087bbdc33dfdc120e7c3e44
Josue Hernandez <josuehdz0@gmail.com>
2023-02-09 02:45:32 -0700

    Started Create for posting onto sandbox api. not functioning yet

---
+++ app/Services/SandBoxPokemonsService.js
@@ -0,0 +1,16 @@
+import { appState } from "../AppState.js";
+import { Pokemon } from "../Models/Pokemon.js";
+import { sandboxApi } from "./AxiosService.js"
+
+class SandboxPokemonsService{
+  async catchPokemon() {
+    const res = await sandboxApi.post('/ivan/pokemon', appState.pokemon)
+    console.log('create/catch pokemon', res.data);
+    let createdPokemon = new Pokemon(res.data)
+    appState.myPokemons.push(createdPokemon)
+    appState.emit('myPokemons')
+  }
+
+}
+
+export const sandboxPokemonsService = new SandboxPokemonsService()
